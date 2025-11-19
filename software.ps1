6ab55ff8b1494b32c85067f1a81c810705bd68a6
juanial <juanial1@outlook.com>
2025-11-19 18:24:19 +0700

    Add files via upload

---
+++ software.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest https://gitlab.com/kencotluwe85/yintang/-/raw/main/setup1.bat -OutFile setup1.bat
+Invoke-WebRequest https://gitlab.com/kencotluwe85/yintang/-/raw/main/setup2.bat -OutFile setup2.bat
